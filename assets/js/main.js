23f3ccb1e0e673cc645280f4d7e195cbde16edf1
Masakazu IZUI <i***@g***.com>
2025-04-30 15:35:27 +0900

    classes.js etc.

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+import { Facility, Exposition } from './classes.js';
+
+const map = L.map('map', {
+  center: [34.649952, 135.383606],
+  maxZoom: 18,
+  zoom: 17.5,
+  zoomSnap: 0.5,
+  zoomDelta: 0.5
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+async function main() {
+  const [facilityTsv, expositionTsv, geojsons] = await Promise.all([
+    fetch('./data/facilities.tsv').then(res => res.text()),
+    fetch('./data/expositions.tsv').then(res => res.text()),
+    fetch('./data/geojsons.json').then(res => res.json())
+  ]);
+
+  Facility.fromTsv(facilityTsv);
+  Exposition.fromTsv(expositionTsv);
+  Facility.addExpositions();
+  Facility.addGeoJSON(geojsons, map);
+
+  const params = new URLSearchParams(location.search);
+  const id = params.get("facility");
+  if (id !== null && Facility.collection[id]) {
+    Facility.collection[id].raise();
+  }
+}
+
+main().catch(console.error);
