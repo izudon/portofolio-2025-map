7ad2c2447781683a205078c51cfe2dbde9bb7b93
Masakazu IZUI <i***@g***.com>
2025-05-02 18:09:21 +0900

    main.js to a module / zoomControl to right-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 import { Facility, Exposition, NDay } from './classes.js';
 
-const map = L.map('map', {
-  center: [34.649952, 135.383606],
-  maxZoom: 18,
-  zoom: 17.5,
-  zoomSnap: 0.5,
-  zoomDelta: 0.5
-});
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
-
 async function main() {
+  const map = L.map('map', {
+    center: [34.649952, 135.383606],
+    maxZoom: 18,
+    zoom: 17.5,
+    zoomSnap: 0.5,
+    zoomDelta: 0.5,
+    zoomControl: false // for let its position change.
+  });
+  L.control.zoom({ position: 'topright' }).addTo(map); // position change
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+
   const [facilityTsv, expositionTsv, geojsons] = await Promise.all([
     fetch('./data/facilities.tsv').then(res => res.text()),
     fetch('./data/expositions.tsv').then(res => res.text()),
